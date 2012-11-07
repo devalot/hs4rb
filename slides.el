363c56a91ba39682efc9f864837a1e860babdacb
Peter Jones <pjones@pmade.com>
2012-11-07 15:08:06 -0700

    Start building slides in Emacs

---
+++ slides.el
@@ -0,0 +1,6 @@
+;;; slides.el -- Slide definitions for bufshow.el
+(bufshow-start
+ [("src/intro.hs" "intro")
+  ("outline.org" "f4b8e166-e1a1-406c-b0db-3f4a64b511d0")
+  ("outline.org" "1d0968dd-4cbb-43a3-8fe4-3d82a092075e")
+  ("src/intro.hs" "square")])
