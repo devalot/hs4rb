752fbcdeb43a498aaca6bc5e8df9114b3fe178f1
Peter Jones <pjones@pmade.com>
2012-11-09 16:05:24 -0700

    Added more material

--- slides.el
+++ slides.el
@@ -1,6 +1,13 @@
 ;;; slides.el -- Slide definitions for bufshow.el
 (bufshow-start
  [("src/intro.hs" "intro")
+  ("outline.org" "868720b6-d686-4eb8-b855-dccae37b1f4f")
   ("outline.org" "f4b8e166-e1a1-406c-b0db-3f4a64b511d0")
   ("outline.org" "1d0968dd-4cbb-43a3-8fe4-3d82a092075e")
-  ("src/intro.hs" "square")])
+  ("src/intro.hs" "functions")
+  ("src/intro.hs" "square")
+  ("src/intro.hs" "square'")
+  ("src/intro.hs" "square''")
+  ("src/intro.hs" "add")
+  ("src/intro.hs" "sum")
+  ("outline.org"  "488e9b3e-ec72-4e9b-8973-97376004d885")])
